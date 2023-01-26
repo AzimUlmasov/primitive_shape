ca0fef34b3e61ff843ce4b5a0b76263df46d4a23
Azim Ulmasov <azimulmasov2@gmail.com>
2023-01-26 21:23:29 +0500

    rect

--- rectangle.py
+++ rectangle.py
@@ -1,4 +1,12 @@
 from polygon import Polygon
 
 class Rectangle(Polygon):
-    pass
+    def getArea(self):
+        return super().getArea()
+    def getPerimeter(self):
+        return super().getPerimeter()
+
+rect =Rectangle(6, 7)
+
+print(rect.getArea())
+print(rect.getPerimeter())
